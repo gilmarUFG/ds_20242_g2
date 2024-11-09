ca76d7b373777ac0444e5a5ae017cf38a9509708
Augusto Moura <me@augustomoura.com>
2024-11-08 21:00:20 -0300

    Adicione app de usuarios

--- Código Fonte/ecoalerta/urls.py
+++ Código Fonte/ecoalerta/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
+    path("usuarios/", include("usuarios.urls")),
 ]
