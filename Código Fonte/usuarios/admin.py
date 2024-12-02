5c50cd9f29b785415934adffef868afdf4c2e566
Augusto Moura <me@augustomoura.com>
2024-12-02 20:18:49 -0300

    Adicione configurações adicionais de usuarios

--- Código Fonte/usuarios/admin.py
+++ Código Fonte/usuarios/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from usuarios.models import Usuario
+
+
+class UsuarioInline(admin.StackedInline):
+    model = Usuario
+    can_delete = False
+    verbose_name_plural = "usuarios"
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [UsuarioInline]
+
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
