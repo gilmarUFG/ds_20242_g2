87f8af28685eb2cb1bfa6d082ce851dfe999213e
Augusto Moura <me@augustomoura.com>
2024-12-02 23:42:20 -0300

    Mapeia campos de CPF corretamente

--- Código Fonte/usuarios/models.py
+++ Código Fonte/usuarios/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from cpf_field.models import CPFField
 
 from polymorphic.models import PolymorphicModel
 
@@ -9,7 +10,7 @@ from polymorphic.models import PolymorphicModel
 class Usuario(PolymorphicModel):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     nome = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=11)
+    cpf = CPFField()
     data_nascimento = models.DateField()
     endereco = models.TextField(max_length=200)
 
